e2a9bb44e3d63e0f67d274a336ad47bcde4471d1
Assia Deramchi <at.deramchi@gmail.com>
2020-07-29 14:04:40 +0100

    schoolservices scaffolded

--- frontend/src/components/SchoolServices.js
+++ frontend/src/components/SchoolServices.js
@@ -1,20 +1,55 @@
 import React from 'react'
-
+import { Carousel } from 'react-responsive-carousel'
+import { homeCarousel } from '../styles/assets/homeCarousel/homeCarousel-data'
 
 const SchoolServices = () => {
   return (
-    <div>
-      <section className="hero is-primary">
+    <div className="school-services">
+      <section className="hero is-warning">
         <div className="hero-body">
           <div className="container">
             <h1 className="title">
-        Primary title
+        School Services
             </h1>
             <h2 className="subtitle">
-        Primary subtitle
+              something or other
             </h2>
           </div>
         </div>
+      </section>
+      <section className="school-services-carousel">
+      <Carousel
+          infiniteLoop
+          centerMode
+          dynamicHeight={true}
+        >
+          {homeCarousel.map(image => <div key={image.name} id={image.name} className="carousel-item" >
+            <img src={image.value} className="carousel-image" />
+          </div>)}
+        </Carousel>
+      </section>
+      <section className="info-and-testimonial">
+        <div className="school-services-info">It's uh, the other end of town, a block past Maple. Why not? Marty, I'm sorry, but the only power source capable of generating one point twenty-one gigawatts of electricity is a bolt of lightning. Who, who? Wait a minute, wait a minute. 1:15 in the morning?
+
+        That Biff, what a character. Always trying to get away with something. Been on top of Biff ever since high school. Although, if it wasn't for him- Yeah, I'm- mayor. Now that's a good idea. I could run for mayor. Oh. I will. I'm too loud. I can't believe it. I'm never gonna get a chance to play in front of anybody.
+
+        I'm, I'm sorry, Mr. McFly, I mean, I was just starting on the second coat. Doc, you gotta help- You wait and see, Mr. Caruthers, I will be mayor and I'll be the most powerful mayor in the history of Hill Valley, and I'm gonna clean up this town. Radiation suit, of course, cause all of the fall out from the atomic wars. This is truly amazing, a portable television studio. No wonder your president has to be an actor, he's gotta look good on television. You too.
+
+        Right. So tell me, Marty, how long have you been in port? His head's gone, it's like it's been erased. Oh, you mean how you're supposed to act on a first date. Jennifer, oh are you a sight for sore eyes. Let me look at you.</div>
+        <div className="divider"></div>
+        <div className="school-services-testimonial">
+        <article class="message is-success">
+        <div class="message-header">
+      <p>Derek Junior, Beekeeper</p>
+      </div>
+      <div class="message-body">
+      <em>Why is she gonna get angry with you? Oh honey, he's teasing you, nobody has two television sets. Great good, good, Lorraine, I had a feeling about you two. Marty, will we ever see you again? Hey boy, are you alright?
+
+      Marty, you didn't fall asleep, did you? C'mon. Yeah. Silence Earthling. my name is Darth Vader. I'm am an extra-terrestrial from the planet Vulcan. I have to tell you about the future.</em>
+    </div>
+    </article>
+        </div>
+        
       </section>
     </div>
 
--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
-<<<<<<< HEAD
 import PhoneLogo from '../../styles/assets/logos/phone-logo.png'
 import EmailLogo from '../../styles/assets/logos/email-logo.png'
-=======
-
->>>>>>> development
 
 const handleClick = (e) => {
   console.log(e.target)
@@ -17,21 +13,12 @@ const Navbar = () => {
       <div className="nav-top">
         <img id="sk-logo" src="https://static.wixstatic.com/media/1a0189_f6d00781a30642748a8c3de78937a259~mv2_d_2480_1265_s_2.png/v1/crop/x_0,y_244,w_2480,h_785/fill/w_600,h_190,al_c,q_85,usm_0.66_1.00_0.01/Sporting%20Kids%20logo%20PNG.webp" />
         <div className="contact">
-<<<<<<< HEAD
           <div className="email">
             <img className="email" src={EmailLogo} alt="email"/>info@sportingkids.com 
           </div>
           <div className="phone-number">
             <img className="phone-number" src={PhoneLogo} alt="phone"/>07930972647
           </div>
-=======
-          {/* <div className="email">
-            <EmailIcon fontSize="large"/>info@sportingkids.com 
-          </div>
-          <div className="phone-number">
-            <CallRoundedIcon fontSize="large"/>07930972647 */}
-          {/* </div> */}
->>>>>>> development
         </div>
       </div>
       <div className="nav-bottom">
