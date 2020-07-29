87c9c41b7f14d4fa5ff5bc2d6ef4946441f3286d
Benga <benga.olasebikan@gmail.com>
2020-07-29 12:39:33 +0100

    about us started

--- frontend/src/components/common/About.js
+++ frontend/src/components/common/About.js
@@ -3,20 +3,64 @@ import React from 'react'
 
 const About = () => {
   return (
-<div>
-<section class="hero is-primary">
-  <div class="hero-body">
-    <div class="container">
-      <h1 class="title">
-        Primary title
-      </h1>
-      <h2 class="subtitle">
-        Primary subtitle
-      </h2>
+    <div className="about">
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
+              ABOUT US
+            </h1>
+            <h2 className="subtitle">
+              Rene and Ola
+            </h2>
+          </div>
+        </div>
+      </section>
+      <div className="columns">
+        <div className="column about-rene">
+          <div classname="card">
+            <div classname="card-image">
+              <figure classname="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/480x320.png" alt="Placeholder image" />
+              </figure>
+            </div>
+            <div classname="card-content">
+              <div classname="media">
+                <div classname="media-content">
+                  <p classname="title is-4">Rene Steer</p>
+                  <p classname="subtitle is-6">@renesteer</p>
+                </div>
+              </div>
+              <div classname="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="column about-ola">
+          <div classname="card">
+            <div classname="card-image">
+              <figure classname="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/480x320.png" alt="Placeholder image" />
+              </figure>
+            </div>
+            <div classname="card-content">
+              <div classname="media">
+                <div classname="media-content">
+                  <p classname="title is-4">Ola Williams</p>
+                  <p classname="subtitle is-6">@olawilliams</p>
+                </div>
+              </div>
+              <div classname="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</section>
-</div>
 
   )
 }
--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
-import CallRoundedIcon from '@material-ui/icons/CallRounded'
-import EmailIcon from '@material-ui/icons/Email'
+
 
 const handleClick = (e) => {
   console.log(e.target)
@@ -13,19 +12,19 @@ const Navbar = () => {
       <div className="nav-top">
         <img id="sk-logo" src="https://static.wixstatic.com/media/1a0189_f6d00781a30642748a8c3de78937a259~mv2_d_2480_1265_s_2.png/v1/crop/x_0,y_244,w_2480,h_785/fill/w_600,h_190,al_c,q_85,usm_0.66_1.00_0.01/Sporting%20Kids%20logo%20PNG.webp" />
         <div className="contact">
-          <div className="email">
+          {/* <div className="email">
             <EmailIcon fontSize="large"/>info@sportingkids.com 
           </div>
           <div className="phone-number">
-            <CallRoundedIcon fontSize="large"/>07930972647
-          </div>
+            <CallRoundedIcon fontSize="large"/>07930972647 */}
+          {/* </div> */}
         </div>
       </div>
       <div className="nav-bottom">
         <div className ="menu-port">
           <div className="links">
-            <Link className="nav-link">Home</Link>
-            <Link className="nav-link">About</Link>
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/about" className="nav-link">About</Link>
             <Link className="nav-link">Multi-sport Services</Link>
             <Link className="nav-link">School Services</Link>
             <Link className="nav-link">Whats On</Link>
