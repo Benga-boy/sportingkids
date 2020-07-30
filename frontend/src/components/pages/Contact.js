7f05fefa83c5df0fd9de404437e078bcc973d863
Benga <benga.olasebikan@gmail.com>
2020-07-31 00:56:39 +0100

    contact form added

--- frontend/src/components/pages/Contact.js
+++ frontend/src/components/pages/Contact.js
@@ -3,7 +3,50 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className="contact-us">
-      CONTACT US
+      <section className="hero is-danger">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title has-text-centered">
+              We will love to hear from you!
+            </h1>
+          </div>
+        </div>
+      </section>
+      <section className="section">
+        <div className="container">
+          <div className="columns is-mobile">
+            <div className="column is-half">
+              <form method="POST" action="https://formspree.io/mknqkgww">
+                <div className="field">
+                  <label className="label">Name</label>
+                  <div className="control">
+                    <input className="input is-medium is-link" type="text" placeholder="Full Name" name="name" required/>
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Email</label>
+                  <div className="control">
+                    <input className="input is-medium is-link" type="text" placeholder="Email Address" name="email" required/>
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Subject</label>
+                  <div className="control">
+                    <input className="input is-medium is-link" type="text" placeholder="Subject" name="subject" required/>
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Message</label>
+                  <div className="control">
+                  <textarea className="textarea is-medium is-link" placeholder="Message" name="message" required/>
+                  </div>
+                </div>
+                <button className="button is-medium is-link">SEND</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
