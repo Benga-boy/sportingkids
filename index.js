14869ddfac67a970e9e4ddd0fb8d2b435aa370e1
Benga <benga.olasebikan@gmail.com>
2020-09-15 01:17:46 +0100

    trying different build code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const router = require('./config/routes')
 const errorHandler = require('./lib/errorHandler')
 const { dbURI, port } = require('./config/environment')
-const path = require('path')
+// const path = require('path')
 require('dotenv').config()
 
 mongoose.connect(
@@ -26,13 +26,17 @@ app.use('/api', router)
 app.use(errorHandler)
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set a static folder
-  app.use(express.static('frontend/build'))
+// if (process.env.NODE_ENV === 'production') {
+//   // Set a static folder
+//   app.use(express.static('frontend/build'))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  })
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//   })
+// }
+
+app.use(express.static(`${__dirname}/frontend/build`))
+
+app.use('/*', (req, res) => res.sendfile(`${__dirname}/frontend/build/index.html`))
 
 app.listen(port, ()=> console.log(`Express is listening on port ${port}`))
